a8eb99b6b8b0ebcbfcb7033b3a781b9488fdec74
Mikegrann <mgrann03@yahoo.com>
2015-07-29 22:36:25 -0700

    Redesigned SharkVertex

--- Aqueous/SharkVertex.h
+++ Aqueous/SharkVertex.h
@@ -5,28 +5,40 @@
 
 class SharkVertex
 {
+	struct SharkVertexBone {
+		SharkVertexBone(string n, float w) : name(n), weight(w)
+		{}
+
+		string name;
+		float weight;
+	};
+
 	public:
-		SharkVertex(){bones = vector<pair<string,float> >();}
-		~SharkVertex(){}
-		glm::vec3 gLocal(){return local; }
-		glm::vec3 gTransformed(){return transformed;}
-		glm::vec3 gNormal(){return normal;}
-		void sNormal(glm::vec3 n){normal = n;}
-		void sTransformed(glm::vec3 n){transformed = n;}
-		void sLocal(glm::vec3 n){local = n;}
-		int gNumInfluences(){return bones.size();}
-		string gBoneName(int index){return bones[index].first;}
-		float gBoneWeight(int index){return bones[index].second;}
-		void sBoneWeight(int index, float newWeight){bones[index].second = newWeight ;}
-		void sBonePair(pair<string,float> f){bones.push_back(f);}
-		void sBonePair(string boneName, float boneWeight){sBonePair(pair<string, float>(boneName, boneWeight));}
+		glm::vec3 gLocal() { return local; }
+		glm::vec3 gTransformed() { return transformed; }
+		glm::vec3 gNormal() { return normal; }
+
+		void sNormal(glm::vec3 n) { normal = n; }
+		void sTransformed(glm::vec3 n) { transformed = n; }
+		void sLocal(glm::vec3 n) { local = n; }
+
+		int gNumInfluences() { return bones.size(); }
+		string gBoneName(int index) { return bones[index].name; }
+		float gBoneWeight(int index) { return bones[index].weight; }
+
+		void sBoneWeight(int index, float newWeight) { bones[index].weight = newWeight; }
+		void sBonePair(pair<string,float> f) { bones.emplace_back(f.first, f.second); }
+		void sBonePair(string boneName, float boneWeight) { bones.emplace_back(boneName, boneWeight); }
+
 		float checkBone(string boneName);
 
 	//private:
 		glm::vec3 local;    
 		glm::vec3 transformed;  //vertex after transformation
 		glm::vec3 normal;
-		vector<pair<string,float> > bones;
+
+	private:
+		vector<SharkVertexBone> bones;
 };
 
 #endif
